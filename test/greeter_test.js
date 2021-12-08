a643512f7f8d9c20e3307f018385f4b051584974
XP.Muhammad.Ullah <XP.Muhammad.Ullah@inovalon.global>
2021-12-08 17:13:40 +0500

    first commit

---
+++ test/greeter_test.js
@@ -0,0 +1,64 @@
+const GreeterContract = artifacts.require("Greeter");
+
+contract("Greeter", (accounts) => {
+  it("has been deployed successfully", async () => {
+    const greeter = await GreeterContract.deployed();
+    assert(greeter, "contract was not deployed");
+  });
+
+  describe("Greeter", () => {
+    it("returns 'Hello, World!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    });
+  });
+
+  describe("Owner", () => {
+    it("returns the address of the owner'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const owner = await greeter.owner();
+
+      assert(owner, "the current owner");
+    });
+
+    it("Matches the address that originally deployed the contract", async () => {
+      const greeter = await GreeterContract.deployed();
+      const owner = await greeter.owner();
+      const expected = accounts[0];
+
+      assert.equal(owner, expected, "matches address used to deploy contract");
+    });
+  });
+});
+
+contract("Greeter : Update Greetings", (accounts) => {
+  describe("setGreeting(string)", () => {
+    it("sets greeting to passed in string", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hi there!";
+
+      await greeter.setGreeting(expected);
+      const actual = await greeter.greet();
+      assert.equal(actual, expected, "greeting was not updated ");
+    });
+  });
+
+  describe("When message is sent by another account", () => {
+    it("does not set greeting", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = await greeter.greet();
+
+      try {
+        await greeter.setGreeting("Not the owner", { from: accounts[1] });
+      } catch (err) {
+        const errorMessage = "Ownable: caller is not the owner";
+        assert.equal(err.reason, errorMessage, "greeting should not update");
+        return;
+      }
+      assert(false, "greeting should not update");
+    });
+  });
+});
